1ed72041e529594545398c3b541b144a22d9d136
Banshee <klindyuk98@gmail.com>
2021-11-23 20:50:45 +0300

    add HW5(Intent For Start BatCalc)

--- app/src/main/java/com/example/startbatcalc/MainActivity.java
+++ app/src/main/java/com/example/startbatcalc/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.startbatcalc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.textview).setOnClickListener(view -> {
+            Intent i = new Intent("startBatCalc");
+            startActivity(i);
+        });
     }
 }
